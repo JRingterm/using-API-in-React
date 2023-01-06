90ce3fd043a671a1611cca124f6ba9750ca0d5f2
JRingterm <ghwns3712@naver.com>
2023-01-07 00:05:39 +0900

    using API in React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [staffs, setStaff] = useState([]);//json 데이터를 보여주기위한 state
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users") //이 주소로 들어가면 해당API에 대한 상세한 정보 존재.
+      .then((response) => response.json()) //response로부터 json 추출
+      .then((json) => {
+        setStaff(json);
+        setLoading(false);
+      }); //그 json을 state로 넘기고, loading을 멈춤
+    }, []); //컴포넌트가 시작할때 한번만 실행.
+
+  //JavaScript를 쓸때는 항상 {}안에 쓰기. 안그러면 그냥 텍스트가 됨!
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Staff! ({staffs.length})</h1> 
+      {loading ? <strong>Loading...</strong> : 
+        <select>
+          {staffs.map((staff) => 
+            <option>
+              {staff.id}. {staff.name} ({staff.username}): {staff.address.street}, {staff.address.suite}
+            </option>
+          )}
+        </select>
+      }
     </div>
   );
-}
-
+} 
 export default App;
